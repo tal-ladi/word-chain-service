a712ffa6025e423d30506e424ce5b7e868003814
Tal Ladi <tallat4556@gmail.com>
2024-09-18 12:00:29 +0300

    Enable SSL for PostgreSQL Connection

--- services/database.js
+++ services/database.js
@@ -2,7 +2,11 @@ const {Pool} = require('pg')
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: false,
+    ssl: {
+        rejectUnauthorized: false,
+      },
+      max: 10,
+      idleTimeoutMillis: 30000,
 });
 
 pool.connect((err, client, release) => {
